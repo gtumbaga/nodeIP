72b915e7a20de4a31a9d9976de137a7c7198a207
Gabriel Tumbaga <gtumbaga@localhost.localdomain>
2018-10-14 06:31:23 +0000

    added extra IP checks

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    const userIP = req.connection.remoteAddress;
+    //const userIP = req.connection.remoteAddress;
+    const userIP = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
+    req.connection.remoteAddress || 
+    req.socket.remoteAddress || 
+    req.connection.socket.remoteAddress;
 
-    res.write(userIP);
+    res.write('IP: ' + userIP);
     res.end();
   }
 });
